b3c2d6d75df4687781e92b24c10e023f7a6e26bf
chaconcastillojoel <joel.chacon@cimat.mx>
2023-01-03 11:47:00 -0600

    ..

---
+++ t2.sh
@@ -0,0 +1,1 @@
+python3 /home/joel.chacon/tmp/WildFire_GCN/Run_Model.py --seed=10000 --rnn_units=16 --embed_dim=64 --link_len=2 --minbatch_size=64 --num_layers=3 > /home/joel.chacon/tmp/WildFire_GCN/gcn_rnn_units_16_embed_dim_64_link_len_2_minbatch_size_64_num_layers_3 2>&1
