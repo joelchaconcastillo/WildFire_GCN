398b552db476098ea4e23d6b5aad418d7e4a9e51
joel <joel.chacon@cimat.mx>
2023-01-03 19:53:25 -0600

    ..

--- t1.sh
+++ t1.sh
@@ -1,1 +1,1 @@
-python3 /home/joel.chacon/tmp/WildFire_GCN/Run_Model.py --seed=10000 --rnn_units=32 --embed_dim=64 --link_len=2 --minbatch_size=64 --num_layers=2 > /home/joel.chacon/tmp/WildFire_GCN/gcn_rnn_units_32_embed_dim_64_link_len_2_minbatch_size_64_num_layers_2 2>&1
+python3 Run_Model.py --seed=10000 --rnn_units=64 --embed_dim=64 --link_len=2 --minbatch_size=64 --num_layers=2 > gcn_rnn_units_64_embed_dim_64_link_len_2_minbatch_size_64_num_layers_2 2>&1
